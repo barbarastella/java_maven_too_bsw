92e6b5d7a9db370cb7544588cb6f4b82bf8306f5
20222PF.CC0003 <20222PF.CC0003@pc-155966.passofundo.ifsul.local>
2023-08-14 09:52:31 -0300

    Teste de alterações em novo commit (14/08/23).

--- src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_bsw.java
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_bsw.java
@@ -9,7 +9,9 @@ package br.edu.ifsul.bcc.too.exercicios.respostas;
  * @author 20222PF.CC0003
  */
 public class Java_maven_too_bsw {
-
+    
+    
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
